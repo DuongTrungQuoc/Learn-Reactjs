9dd9bd05bdd1f1ca315c5bd4745c40d79d061915
dtquoc1202 <quoccccc36@gmail.com>
2024-11-22 17:08:09 +0700

    22/11

--- thamSoConLai.js
+++ thamSoConLai.js
@@ -1,13 +1,12 @@
 // Rest parameters (Tham số "còn lại") là tham số đại diện cho những tham số không được khai báo của một function.
 // Được ký hiệu bằng khai báo ...name (cẩn thận nhầm với spread syntax - dùng trong array và object).
 
-// const sum = (...number) => {
-//   const result = number.reduce((total, item) => {
-//     return total + item;
-//   }, 0);
-//   return result;
-// };
+const sum = (...number) => {
+  const result = number.reduce((total, item) => {
+    return total + item;
+  }, 0);
+  return result;
+};
 
-// console.log(sum(1, 2, 3, 4, 5));
+console.log(sum(1, 2, 3, 4, 5));
 
-console.log("hello");
